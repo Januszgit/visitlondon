d115fd6208689c8af788bc4625ec82aa3158dddc
Januszgit <jbartecki@gmail.com>
2017-05-29 17:44:48 +0200

    merge stream added

---
+++ src/js/scripts.js
@@ -0,0 +1,9 @@
+//browserify syntax
+
+global.jQuery = require('jquery');
+bootstrap = require('bootstrap');
+mustache = require('mustache');
+
+jQuery(document).ready( function() {
+  alert("hello");
+});
